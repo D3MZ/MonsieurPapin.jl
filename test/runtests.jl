b81f42d5f34df98573a4d9c8d07e694a7c4251fc
Demetrius Michael <arrrwalktheplank@gmail.com>
2026-02-27 23:24:42 -0500

    Add wet URI allocation test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,8 @@ using MonsieurPapin
 using Test
 
 @testset "MonsieurPapin.jl" begin
-
+    path = joinpath(@__DIR__, "data", "wet.paths.gz")
+    channel = wetURIs(path)
+    allocs = @allocations first(channel)
+    @test allocs == 1
 end
