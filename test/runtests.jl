994dec8c25ed0b0ce7968f4ca2b1924ea859348c
Demetrius Michael <arrrwalktheplank@gmail.com>
2026-03-03 12:45:29 -0500

    updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,3 +11,32 @@ using HTTP: URI
     channel = wets(path)
     @test @allocations(first(channel)) == 1 #250.000 ns  Memory estimate: 224 bytes, allocs estimate: 1.
 end
+
+
+# using CodecZlib
+# path = joinpath(@__DIR__, "test/data", "warc.wet.gz")
+# open(path) do file
+#     stream = GzipDecompressorStream(file)
+#     @allocations readline(stream)
+# end
+
+# using CodecZlib
+
+# open(path) do file
+#     stream = GzipDecompressorStream(file)
+#     total = 0
+#     while !eof(stream)
+#         total += @allocations readline(stream)
+#     end
+#     total
+# end
+
+# @allocations begin
+#     for _ in wets(path)
+#     end
+# end
+
+using BenchmarkTools
+
+display(@benchmark sum(_ -> 1, channel) setup=(path = joinpath(@__DIR__, "test", "data", "warc.wet.gz"); channel = wets(path)) evals=1)
+
