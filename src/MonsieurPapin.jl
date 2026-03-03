32f5b1df788d0aa367698620e4b1d607ba8214eb
Demetrius Michael <arrrwalktheplank@gmail.com>
2026-03-03 15:03:06 -0500

    Add embedding-based relevance dispatch

--- src/MonsieurPapin.jl
+++ src/MonsieurPapin.jl
@@ -3,7 +3,7 @@ module MonsieurPapin
 using HTTP, CodecZlib, BufferedStreams, Dates, LinearAlgebra
 using HTTP: URI
 
-export WET, wetURIs, wets, datadir, isrelevant
+export WET, wetURIs, wets, datadir, Embedding, embedding, isrelevant
 
 const capacity = 10
 const wetroot = "https://data.commoncrawl.org/"
