2477ccd8185de1e9d69d87dbec202d19ebb6a248
Demetrius Michael <arrrwalktheplank@gmail.com>
2026-03-11 22:54:48 -0400

    refactor

--- test/benchmarks.jl
+++ test/benchmarks.jl
@@ -1,10 +1,10 @@
 # DO ADD OR REMOVE COMMENTS FROM THIS FILE
-using MonsieurPapin, BenchmarkTools, Test
-import MonsieurPapin: insert!
+using MonsieurPapin, BenchmarkTools, Statistics, Test
+import MonsieurPapin: insert!, score
 
 # USE REAL DATASETS, NOT SIMULATED FOR BENCHMARKING.
-path_weturis = joinpath(dirname(@__DIR__), "data", "wet.paths.gz")
-path_wets = joinpath(dirname(@__DIR__), "data", "warc.wet.gz")
+urispath = joinpath(dirname(@__DIR__), "data", "wet.paths.gz")
+wetspath = joinpath(dirname(@__DIR__), "data", "warc.wet.gz")
 model_source = "minishlab/potion-multilingual-128M"
 
 count(iterable) = sum(_ -> 1, iterable)
@@ -12,42 +12,43 @@ rate(iterable, seconds) = round(count(iterable) / seconds)
 
 @testset "benchmarks" begin
     @testset "wetURIs" begin
-        benchmark = @benchmark sum(_ -> 1, wetURIs($path_weturis))
-        time = BenchmarkTools.median(benchmark).time / 1e9
+        benchmark = @benchmark sum(_ -> 1, wetURIs($urispath))
+        time = median(benchmark).time / 1e9
         display(benchmark)
         @test benchmark.allocs <= 5 * 100_000 # less than 5 allocations per record (at 100K records)
-        uris_per_second = rate(wetURIs(path_weturis), time)
-        @info "Benchmarking wetURIs (paths)" uris = count(wetURIs(path_weturis)) uris_per_second = uris_per_second
+        uris_per_second = rate(wetURIs(urispath), time)
+        @info "Benchmarking wetURIs (paths)" uris = count(wetURIs(urispath)) uris_per_second = uris_per_second
     end
 
     @testset "wets" begin
-        benchmark = @benchmark sum(_ -> 1, wets($path_wets))
-        time = BenchmarkTools.median(benchmark).time / 1e9
+        benchmark = @benchmark sum(_ -> 1, wets($wetspath))
+        time = median(benchmark).time / 1e9
         display(benchmark)
         @test benchmark.allocs <= 75_000 # less than 3 allocations per record (~24K pages)
-        records_per_second = rate(wets(path_wets), time)
-        @info "Benchmarking wets (records)" records = count(wets(path_wets)) records_per_second = records_per_second
+        records_per_second = rate(wets(wetspath), time)
+        @info "Benchmarking wets (records)" records = count(wets(wetspath)) records_per_second = records_per_second
         @test records_per_second >= 25_000
     end
 
-    @testset "relevant!" begin
-        source = embedding("cat dog"; vecpath=model_source)
-        benchmark = @benchmark sum(_ -> 1, relevant!($source, wets($path_wets); threshold=0.0))
-        time = BenchmarkTools.median(benchmark).time / 1e9
+    @testset "Multilingual Distance between Query and WET" begin
+        benchmark = @benchmark score(source, take!(channel)) setup=(
+            source = embedding("cat dog"; vecpath=model_source);
+            channel = wets(wetspath)
+        ) evals=1
+        time = median(benchmark).time / 1e9
         display(benchmark)
-        records_per_second = rate(relevant!(source, wets(path_wets); threshold=0.0), time)
-        @info "Benchmarking relevant! (records)" records = count(relevant!(source, wets(path_wets); threshold=0.0)) records_per_second = records_per_second
-        # @test records_per_second >= 25_000
+        @info "Benchmarking relevant! (records)" records_per_second = round(1 / time)
+        @test records_per_second >= 5_000
     end
 
     @testset "Queuing the top 1K" begin
-        benchmark = @benchmark insert!(queue, wets($path_wets)) setup=(queue = WETQueue(1_000, typeof(first(wets($path_wets))))) evals=1
-        time = BenchmarkTools.median(benchmark).time / 1e9
+        benchmark = @benchmark insert!(queue, wets($wetspath)) setup=(queue = WETQueue(1_000, typeof(first(wets($wetspath))))) evals=1
+        time = median(benchmark).time / 1e9
         display(benchmark)
-        queue = WETQueue(1_000, typeof(first(wets(path_wets))))
-        insert!(queue, wets(path_wets))
-        records_per_second = rate(wets(path_wets), time)
-        @info "Benchmarking queue (records)" records = count(wets(path_wets)) capacity = 1_000 retained = length(queue) records_per_second = records_per_second payload_mib = round(Base.summarysize(queue.heap) / 2.0^20; digits=4)
+        queue = WETQueue(1_000, typeof(first(wets(wetspath))))
+        insert!(queue, wets(wetspath))
+        records_per_second = rate(wets(wetspath), time)
+        @info "Benchmarking queue (records)" records = count(wets(wetspath)) capacity = 1_000 retained = length(queue) records_per_second = records_per_second payload_mib = round(Base.summarysize(queue.heap) / 2.0^20; digits=4)
         @test records_per_second >= 20_000
     end
 end
