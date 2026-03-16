41797b1e0ff7b86f7b0e10d0142de3fd14cbf2db
Demetrius Michael <arrrwalktheplank@gmail.com>
2026-03-16 18:19:00 -0400

    Remove daachorse matcher path

--- src/MonsieurPapin.jl
+++ src/MonsieurPapin.jl
@@ -4,13 +4,13 @@ using HTTP, CodecZlib, BufferedStreams, Dates, JSON, StringViews
 using DataStructures: BinaryHeap
 using HTTP: URI
 
-export WET, WETQueue, wetURIs, wets, Configuration, Embedding, embedding, distance, score, complete, translate, gettext, language, languages, isrelevant, relevant!, best!, best, research, AC, DAAC, simhash, Deduper, isduplicate
+export WET, WETQueue, wetURIs, wets, Configuration, Embedding, embedding, distance, score, complete, translate, gettext, language, languages, isrelevant, relevant!, best!, best, research, AC, simhash, Deduper, isduplicate
 
 
 include("wetURIs.jl")
 include("wets.jl")
 include("RustWorker.jl")
-using .RustWorker: AC, DAAC, ismatch
+using .RustWorker: AC
 include("scoring.jl")
 include("simhash.jl")
 include("queue.jl")
