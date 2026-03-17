3e46c18f39d311018bbde91e3ef75e8298dae9c0
Demetrius Michael <arrrwalktheplank@gmail.com>
2026-03-17 16:20:20 -0400

    added progress meter, but might be bugged in terms of time estimate, seems to always increase. Need to investigate more.

--- src/wetURIs.jl
+++ src/wetURIs.jl
@@ -1,22 +1,27 @@
+using ProgressMeter
+
 function wetURIs(path::AbstractString; delimiator=codeunits("\n")[1], capacity=Threads.nthreads()*2)
-    Channel{StringView}(capacity) do uris
+    Channel{String}(capacity, spawn=true) do uris
         open(path) do file
             stream = GzipDecompressorStream(file)
             while !eof(stream)
-                put!(uris, StringView(readuntil(stream, delimiator; keep=false)))
+                put!(uris, String(readuntil(stream, delimiator; keep=false)))
             end
         end
     end
 end
 
 function wetURIs(index::URI; delimiator=codeunits("\n")[1], capacity=Threads.nthreads()*2)
-    Channel{StringView}(capacity) do uris
-        HTTP.open("GET", string(index)) do stream
+    Channel{String}(capacity, spawn=true) do uris
+        progressbar = Progress(100_000; dt=1)
+        HTTP.open("GET", index) do stream
             HTTP.startread(stream)
             gzip = GzipDecompressorStream(BufferedInputStream(stream))
             while !eof(gzip)
-                put!(uris, StringView(readuntil(gzip, delimiator; keep=false)))
+                put!(uris, String(readuntil(gzip, delimiator; keep=false)))
+                next!(progressbar)
             end
         end
+        finish!(progressbar)
     end
 end
